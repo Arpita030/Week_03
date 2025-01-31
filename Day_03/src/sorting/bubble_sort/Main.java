96bbef6990d9be69448de03733ced99505588d88
Arpita030 <0111as211013.arpita@gmail.com>
2025-01-31 16:24:15 +0530

    Question_01

---
+++ Day_03/src/sorting/bubble_sort/Main.java
@@ -0,0 +1,11 @@
+package sorting.bubble_sort;
+import java.util.*;
+public class Main{
+public static void main(String[] args){
+    int []arr={98,56,67,78,90};
+    StudentMark.studentmark(arr);
+    System.out.println(Arrays.toString(arr));
+}
+}
+
+
