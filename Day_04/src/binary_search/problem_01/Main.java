1e4b0d45be330a73a1e21b7c88351ebdd3d1637b
Arpita030 <0111as211013.arpita@gmail.com>
2025-02-05 22:25:46 +0530

    [Arpita]Add.adding Binary Search problem_01

---
+++ Day_04/src/binary_search/problem_01/Main.java
@@ -0,0 +1,10 @@
+package binary_search.problem_01;
+
+public class Main {
+    public static void main(String[] args) {
+        int arr[]={4,5,6,1};
+        int result=RotationPoint.rotation(arr);
+        System.out.println(result);
+
+    }
+}
