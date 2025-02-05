ec4b2316300f7816fc260c32b49c33ed53bb3a37
Arpita030 <0111as211013.arpita@gmail.com>
2025-02-05 22:26:04 +0530

    [Arpita]Add.adding Binary Search problem_02

---
+++ Day_04/src/binary_search/problem_02/Main.java
@@ -0,0 +1,9 @@
+package binary_search.problem_02;
+
+public class Main {
+    public static void main(String[] args) {
+    int[] arr={2,4,56,2,58,7,1};
+    int result =PeakElement.peakelement(arr);
+    System.out.println(result);
+}
+}
